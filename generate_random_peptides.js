f8ebc3a70319c9c782504a9a71c8f82b2d17713b
Leon Bichmann <bichmann@informatik.uni-tuebingen.de>
2020-01-16 14:04:28 +0100

    random shuffle

--- generate_random_peptides.js
+++ generate_random_peptides.js
@@ -1,15 +1,18 @@
+d3 = require('d3')
 peptide='RANDIMPEPTIDETEST'
 
 randomizer = function(peptide){
 	pepl=peptide.length
-	pepidxs=Array.from(new Array(pepl-2), (x,i) => i + 1).reverse()
-	pepidxs.push(pepl-1)
-	pepidxs.unshift(0)
+	pepidxs=Array.from(new Array(pepl-2), (x,i) => i + 1)
+	pepidxs_shuffle=d3.shuffle(pepidxs)
+	pepidxs_shuffle.push(pepl-1)
+	pepidxs_shuffle.unshift(0)
 	chars = peptide.split('')
-	new_chars = pepidxs.map(x => chars[x])
+	new_chars = pepidxs_shuffle.map(x => chars[x])
 	new_peptide=new_chars.join('')
 
 	return new_peptide
 }
 
 console.log(randomizer(peptide))
+
